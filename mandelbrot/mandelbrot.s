cd2377d17bf11fcbf815babf2a38794b0478913c
Graeme Winter <graeme.winter@gmail.com>
2023-02-18 17:13:58 +0000

    Add image §; fix syntax

--- mandelbrot/mandelbrot.s
+++ mandelbrot/mandelbrot.s
@@ -9,7 +9,8 @@
 	@ r4 imag part of z
 	@ r5 tmp / scratch to alias zr
 	@ r6, r7 for the real and imaginary counters (increment by 0x8000)
-	@ r8, r9 for working cr, ci
+	@ r8, r9 for zr2, zi2
+	@ r10, r11 for working space for SMULL instruction
 
 	@ data are stored in 7.24 format fixed point
 	@ domain in real is -2 to 0.5, imag -1.25 to +1.25
@@ -28,8 +29,8 @@ _start:
 imag:
 	mov R6, #0
 real:	
-	add R8, R1, R6, LSL 15
-	add R9, R2. R7, LSL 15
+	add R8, R1, R6, LSL #15
+	add R9, R2, R7, LSL #15
 
 	@ set up for iter
 	mov R0, #0
@@ -50,7 +51,7 @@ iter:
 
 	@ sum and cmp
 	add R5, R8, R9
-	cmp R5, #4, LSL 24
+	cmp R5, #67108864
 	bgt end
 
 	@ next zr
@@ -70,16 +71,26 @@ iter:
 	bne iter
 
 end:
-	
-	mov R0, #0
-        ldr R1, =hello
-        mov R2, #13
-        mov R7, #4
-        svc 0
 
+	@ save value
+
+	@ increment real
+
+	@ real < limit? next real
+
+	@ increment imag
+
+	@ imag < limit? next imag
+
+	@ stop
+
+	@ end
+
+	
         mov R0, #0
         mov R7, #1
         svc 0
 
-.data
-hello: .ascii "Hello world!\n"
+.bss
+image:	 .skip 6553600
+	
