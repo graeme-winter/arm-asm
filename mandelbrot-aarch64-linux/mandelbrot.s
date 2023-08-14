d65a2d053b3ccf93eb87449ea90afd3ca06dd44a
Graeme Winter <graeme.winter@gmail.com>
2023-08-14 06:40:32 +0100

    Optimisation 1
    smull faster than mul with 64 bit operands; cmp with register

--- mandelbrot-aarch64-linux/mandelbrot.s
+++ mandelbrot-aarch64-linux/mandelbrot.s
@@ -16,66 +16,69 @@
 _start:
 	ldr W10, =image
 
+	// 4 << 24 for cmp
+	mov W11, #4
+	lsl W11, W11, #24
+
 	// initial values for ci - origin as above + 0.5 x box
-	mov X2, #-5
-	lsl X2, X2, #22
-	add X2, X2, #0x4000
+	mov W2, #-5
+	lsl W2, W2, #22
+	add W2, W2, #0x4000
 
 imag:
 	// initial values for cr - origin as above + 0.5 x box
-	mov X1, #-2
-	lsl X1, X1, #24
-	add X1, X1, #0x4000
+	mov W1, #-2
+	lsl W1, W1, #24
+	add W1, W1, #0x4000
 
 real:
 	// set up for iter - count, zr, zi
-	mov X0, #0
-	mov X3, #0
-	mov X4, #0
+	mov W0, #0
+	mov W3, #0
+	mov W4, #0
 
 iter:
 	// zr^2
-	mul X8, X3, X3
+	smull X8, W3, W3
 	asr X8, X8, #24
 
 	// zi^2
-	mul X9, X4, X4
+	smull X9, W4, W4
 	asr X9, X9, #24
 
-	// sum and cmp for zr^2 + zi^2 - N.B. the cmp needs the shift
-	add X5, X8, X9
-	asr X5, X5, #24
-	cmp X5, #4
+	// sum and cmp for zr^2 + zi^2
+	add W5, W8, W9
+	cmp W5, W11
 	bge end
 
 	// next zr
-	mov X5, X3
-	sub X6, X8, X9
-	add X3, X6, X1
+	mov W5, W3
+	sub W6, W8, W9
+	add W3, W6, W1
 
 	// next zi
-	mul X6, X5, X4
+	smull X6, W5, W4
 	asr X6, X6, #24
-	add X4, X2, X6, lsl #1
+	add W4, W2, W6, lsl #1
 
-	add X0, X0, #1
-	cmp X0, #0x1000
+	add W0, W0, #1
+	cmp W0, #0x1000
 	bne iter
 
 end:
 	// save value
 	str W0, [X10, #0]
-	add X10, X10, #4
+	add W10, W10, #4
 
 	// increment real, continue - imm allowed
-	add X1, X1, #0x8000
-	cmp X1, #0x800000
+	add W1, W1, #0x8000
+	cmp W1, #0x800000
 	blt real
 
 	// increment imag, continue - N.B. some annoying shifts for imm
-	add X2, X2, #0x8000
-	asr X5, X2, #22
-	cmp X5, #5
+	add W2, W2, #0x8000
+	asr W5, W2, #22
+	cmp W5, #5
 	blt imag
 
 	// write out array
