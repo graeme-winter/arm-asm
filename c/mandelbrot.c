4cac8be34dc8b2a5f685382b9ec7fea2f5ccb119
Graeme Winter <graeme.winter@gmail.com>
2024-02-11 09:12:07 +0000

    Optimise: do not need tmp

--- c/mandelbrot.c
+++ c/mandelbrot.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     for (cr = -((2 << 24) - 0x4000); cr < 0x800000; cr += 0x8000) {
       int32_t count = 0;
       int32_t zr = 0, zi = 0;
-      int32_t tmp, zr2, zi2;
+      int32_t zr2, zi2;
 
       while (count != 0x1000) {
         zr2 = mul(zr, zr);
@@ -19,9 +19,8 @@ int main(int argc, char **argv) {
         if ((zr2 + zi2) >= 0x4000000)
           break;
         count++;
-        tmp = zr;
+        zi = 2 * mul(zr, zi) + ci;
         zr = zr2 - zi2 + cr;
-        zi = 2 * mul(tmp, zi) + ci;
       }
 
       iter[offset] = count;
